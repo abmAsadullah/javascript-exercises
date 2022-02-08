d1f349d6d2e4981e7304065076957854bf41099f
A B M Asadullah <abmasadullah@gmail.com>
2022-02-08 08:21:41 +0600

    Solve repeatd String

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let text = "";
+    for(let i=0; i<num; i++){
+        text += str;
+    }
+    if(num<0) 
+        return "ERROR";
+    else return text;
 };
 
 // Do not edit below this line
